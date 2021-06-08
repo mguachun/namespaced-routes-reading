b57c4e5107506ac3cdf89e73e5602d3ebb5fb7ba
Melissa Guachun <1212malka@gmail.com>
2021-06-07 21:22:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  scope '/admin' do
+    resources :stats, only: [:index]
+  end 
+  
 
   root 'posts#index'
+
+
+  get '/admin/stats', to: 'stats#index' 
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end 
 end
